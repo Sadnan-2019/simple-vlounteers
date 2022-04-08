8a8a61c5f34f62953e630049bded4377823f6a99
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-04-08 22:17:59 +0600

    search add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,6 +14,7 @@ const Home = () => {
                          <h1>This is home</h1>
                          <h5>This is count:{count}</h5>
                          <h4>This is Double Count:{count * 2}</h4>
+                         <h4>This is Double Six Count:{count * 6}</h4>
                          <button onClick={ClickMe}>Click me</button>     
                     </div>
           );
